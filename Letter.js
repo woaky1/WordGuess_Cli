ecde6ff90f014c0b2c3495a92ade62e81c90d28e
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-03 15:48:21 -0500

    Have the display method working in Letter. Committing progress so far.

--- Letter.js
+++ Letter.js
@@ -1,3 +1,23 @@
-function Letter(){
-    
+function Letter(trueLetter,guessed,userGuess){
+    this.trueLetter = trueLetter;
+    this.guessed = guessed;
+    this.userLetter = userGuess;
+    this.display = function() {
+        if (this.guessed === true) {
+            return this.trueLetter;
+        } else {
+            return "_";
+        };
+    };
+    // this.check = function(userGuess){
+    //     if (this.trueLetter === this.userLetter) {
+    //         this.guessed = true;
+    //     } else {
+    //         this.guessed = false;
+    //     }
+    // };
 }
+
+var a = new Letter("a",true,"b");
+console.log(a.display());
+module.exports = Letter;
