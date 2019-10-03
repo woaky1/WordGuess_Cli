c23c52b058c020f05760958b8f55d28ed63a04d1
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-03 16:14:43 -0500

    Think I've got the check function working. That should wrap up Letter.js, for now at least.

--- Letter.js
+++ Letter.js
@@ -1,7 +1,6 @@
-function Letter(trueLetter,guessed,userGuess){
+function Letter(trueLetter,guessed){
     this.trueLetter = trueLetter;
     this.guessed = guessed;
-    this.userLetter = userGuess;
     this.display = function() {
         if (this.guessed === true) {
             return this.trueLetter;
@@ -9,15 +8,13 @@ function Letter(trueLetter,guessed,userGuess){
             return "_";
         };
     };
-    // this.check = function(userGuess){
-    //     if (this.trueLetter === this.userLetter) {
-    //         this.guessed = true;
-    //     } else {
-    //         this.guessed = false;
-    //     }
-    // };
+    this.check = function(userGuess){
+        if (this.trueLetter === userGuess) {
+            this.guessed = true;
+        } else {
+            this.guessed = false;
+        }
+    };
 }
 
-var a = new Letter("a",true,"b");
-console.log(a.display());
 module.exports = Letter;
