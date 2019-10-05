ecc5a2e04d4dc7bc5976a52c5e7841acaf5c4477
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-04 20:25:45 -0500

    Added some inline documentation.

--- Letter.js
+++ Letter.js
@@ -1,6 +1,8 @@
+// This constructor creates an object for each letter in the word we're guessing.
 function Letter(trueLetter){
     this.trueLetter = trueLetter;
     this.guessed = false;
+    // This method tells the game whether to display an underscore or the true letter if the user has guessed this letter correctly.
     this.display = function() {
         if (this.guessed === true) {
             return this.trueLetter;
@@ -8,6 +10,7 @@ function Letter(trueLetter){
             return "_";
         };
     };
+    // This method checks the user's guess against the letter and sees if the user's guess matches. 
     this.check = function(userGuess){
         if (this.guessed === true) {
 
--- Word.js
+++ Word.js
@@ -1,26 +1,30 @@
+// We need to require Letter.js, since this constructor uses methods in that constructor extensively.
 var Letter = require("./Letter.js");
 
+// This constructor takes the string of the word supllied to it and contructs an object that will be used for this round of game play.
 function Word(string) {
     this.string = string;
     this.letters = [];
+    // This method takes the original sting of the word and splits it into seperate letters and feeds it into the Letter constructor, then stores the resulting objects in an array.
     this.letterfy = function(){
         var letterFeed = this.string.split("");
         for (var i = 0; i < letterFeed.length; i++) {
             this.letters.push(letter = new Letter(letterFeed[i]))
         }
     };
+    // This method is used to display the current word in play. Unguessed letters appear as underscores, guessed letters appear correctly.
     this.project = function(){
         var displayArray = [];
         var letterDisplayArray = this.letters
         for (var j = 0; j < letterDisplayArray.length; j++) {
             displayArray.push(letterDisplayArray[j].display());
-            
         }
         var displayString = displayArray.toString().replace(/,/g," ");
         console.log(displayString);
         return displayString;
         
     }
+    // This method cheecks the user's guess against every letter in the word and see if it matches any of them.
     this.wordCheck = function (userGuess) {
         for (var k = 0; k < this.letters.length; k++) {
             this.letters[k].check(userGuess);
